3b1f2d77ad174f0a5d5089718ea8121c90dc2173
iymdtyigsy <139928096+iymdtyigsy@users.noreply.github.com>
2025-05-15 15:11:43 +1200

    working on login gui

--- Login.py
+++ Login.py
@@ -12,34 +12,47 @@ class LoginMenu(ctk.CTk):
         super().__init__()
 
         self.title("Budget app")
-        self.geometry("700x500")
-        self.minsize(700, 500)
-        self.maxsize(700, 500)
+        self.geometry("844x844")
+        self.minsize(844, 844)
+        self.maxsize(844, 844)
 
-        self.mainframe = ctk.CTkFrame(self, fg_color="black",
-                                      width=700,
-                                      height=500)
+        self.mainframe = ctk.CTkFrame(self,
+                                      fg_color="black",
+                                      width=744,
+                                      height=744)
         self.mainframe.pack(fill="both",
                             expand=True)
 
-        self.main_frame_label = (ctk.CTkLabel
-                                 (self.mainframe,
-                                  text="Login",
-                                  text_color="white",
-                                  fg_color="#01a6f8",
-                                  width=200, height=25,
-                                  font=("Bold", 20),
-                                  corner_radius=5))
-        self.main_frame_label.pack(padx=10,
-                                   pady=10)
+        self.mainframe_holder = ctk.CTkFrame(self.mainframe, 
+                                             fg_color="#D9D9D9",
+                                             width=644,
+                                             height=644)
+        self.mainframe_holder.pack(padx=10,
+                                   pady=10,
+                                   fill="both",
+                                   expand=True)
 
-        self.main_frame_holder = ctk.CTkFrame(self.mainframe,
-                                              width=600,
-                                              height=400)
-        self.main_frame_holder.pack(padx=10,
-                                    pady=10,
-                                    fill="both",
-                                    expand=True)
+        self.loginframe = ctk.CTkFrame(self.mainframe_holder,
+                                       fg_color="white",
+                                       width=605,
+                                       height=500)
+        self.loginframe.pack(padx=10,
+                             pady=10,
+                             expand=True)
+        self.loginframe.pack_propagate(False)
+
+        self.loginframe_label = ctk.CTkLabel(self.loginframe,
+                                             text="LOG IN",
+                                             text_color="black",
+                                             fg_color="#FFFEFE",
+                                             width=200, 
+                                             height=25,
+                                             font=("Bold", 20),
+                                             corner_radius=5)
+        self.loginframe_label.pack(padx=10,
+                                   pady=10,
+                                   expand=True)
+        
 
         #self.load_main_menu()
     
