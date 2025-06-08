1af785f2383f1bb9f8708a6b9a63ed3c4fbdf93b
iymdtyigsy <139928096+iymdtyigsy@users.noreply.github.com>
2025-06-09 09:59:52 +1200

    using sqlalchemy

--- Database.py
+++ Database.py
@@ -1,33 +1,20 @@
-import sqlite3
-
 from sqlalchemy import Column, Integer, String, ForeignKey, Sequence, create_engine
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base
 
 import bcrypt
 
-engine = create_engine('')
-def create_conn():
-    conn = sqlite3.connect("data/Account.db") 
-    return conn
+engine = create_engine('sqlite:///accountdatabase.db', echo=True)
 
-def init_database():
-    conn = create_conn()
-    c = conn.cursor()
+Session = sessionmaker(bind=engine)
+session = Session()
 
-    c.execute('''
-    CREATE TABLE IF NOT EXISTS accounts(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )    
-    ''')
+Base = declarative_base()
 
-    conn.commit()
-    conn.close()
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    
+    
 
-def signup(username, password):
-    conn = create_conn()
-    c = conn.cursor()
 
     
